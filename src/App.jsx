762936c343de2fb08e23e6b0495289180c826b30
Omsurvase24 <omsurvase24@gmail.com>
2024-04-17 17:20:51 +0530

    introduction

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./Header"
+import Footer from "./Footer";
+import Food from "./Food";
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Food></Food>
+      <Footer></Footer>
+    </>
+  );
+}
+
+export default App
