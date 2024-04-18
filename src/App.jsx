0e427c952d443468b78b14dd9edd047b60dce34c
Omsurvase24 <omsurvase24@gmail.com>
2024-04-19 03:18:09 +0530

    card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import Header from "./Header"
-import Footer from "./Footer";
-import Food from "./Food";
+import Card from "./Card";
 
 function App() {
   return (
     <>
-      <Header></Header>
-      <Food></Food>
-      <Footer></Footer>
+      <Card></Card>
+      <Card></Card>
     </>
   );
 }
---
+++ src/Card.jsx
@@ -0,0 +1,13 @@
+
+
+function Card() {
+    return (
+        <div className="card">
+            <img clasName="card-image" src="https://via.placeholder.com/150" alt="profile picture"></img>
+            <h2 className="card-title" >Om Survase</h2>
+            <p className="card-text" >I am a Django Backend Developer learning React</p>
+        </div>
+    );
+}
+
+export default Card
