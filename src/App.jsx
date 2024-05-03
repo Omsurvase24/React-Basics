e6a03a4ec573452513bf4115514c75f688ebe6ed
Omsurvase24 <omsurvase24@gmail.com>
2024-05-03 15:11:12 +0530

    props

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import Button from "./Button";
+import Student from "./Student";
 
 function App() {
   return (
     <>
-      <Button></Button>
+      <Student name="Spongebob" age={30} isStudent={true}></Student>
+      <Student name="Patrick" age={39} isStudent={true}></Student>
+      <Student name="Squidward" age={50} isStudent={false}></Student>
+      <Student></Student>
     </>
   );
 }
