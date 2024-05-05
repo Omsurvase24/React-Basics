4c0bcbc3728cecf6faf2953d030699fb000d6fb1
Omsurvase24 <omsurvase24@gmail.com>
2024-05-05 19:19:54 +0530

    Lists

--- src/List.jsx
+++ src/List.jsx
@@ -1,6 +1,8 @@
 function List() {
     const fruits = [{ name: "apple", calories: 95 }, { name: "orange", calories: 45 }, { name: "banana", calories: 105 }, { name: "coconut", calories: 159 }, { name: "pineapple", calories: 37 }];
 
+    const lowCalFruits = fruits.filter(fruit => fruit.calories < 100);
+
     fruits.sort((a, b) => a.name.localeCompare(b.name));
     const listItems = fruits.map(fruit => <li key={fruit.name}>{fruit.name}: &nbsp; <b>{fruit.calories}</b></li>);
 
